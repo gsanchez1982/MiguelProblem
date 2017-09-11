7b6c8881d210c052b17382661e25893995b8d3f3
Guillermo <gsanchez1982@gmail.com>
2017-09-11 07:28:58 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@
 
 
 # ANSWER BELOW:
-# By Guillermo Sánchez Estrada
+# By Guillermo Sanchez Estrada
 # Of Monterrey, Mexico
 
 # First, we make a list of all numbers that matches the characteristics:
@@ -24,12 +24,46 @@ for a in range(1,8):
                         for g in range(1,8):
                             n_set.append(int(str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+str(g)))
 
-# Then we count them.
+# Then, we remove all items from the list that contain repeated digits.
+
+i = '1234567'
+
+x = []
 
 n_len = len(n_set)
 
-# Then we divide the count by two and print the number in the middle.
+while n_len > 0:
+    n_len = n_len - 1
+    for n in i:
+        count = str(n_set[n_len]).count(n)
+        if count > 1:
+            x.append(n_len)
 
-n_mid = n_len / 2
+x = list(set(x))
 
-print(n_set[n_mid])
+x = sorted(x, reverse=True)
+
+for i in x:
+    del n_set[i]
+
+
+# Then we count all numbers in the new list.
+
+n_len = len(n_set)
+
+
+# Then we divide the count by two and divide into two sets of equal parts.
+
+n_mid = int(n_len / 2)
+
+h1 = n_mid - 1
+h2 = (n_mid * 2) - 1
+
+n1_set = n_set[0:h1]
+n2_set = n_set[n_mid:h2]
+
+# Finally, we print out the last number of the first half.
+
+print(n1_set[-1])
+
+# Answer printed out: 4376512
